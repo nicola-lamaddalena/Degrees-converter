04e6829fcaf3758957abb81ed66988bddcc8d0a2
Uomonero1 <niks9498@gmail.com>
2023-08-05 12:07:02 +0200

    add error handling and new tests.

--- main.py
+++ main.py
@@ -1,24 +1,50 @@
 class Converter:
-    def celFah(self, celsius: float) -> float:
-        fahrenheit = round((celsius * (9 / 5)) + 32, 2)
-        return fahrenheit
+    error = "Couldn't convert string to float."
 
-    def fahCel(self, fahrenheit: float) -> float:
-        celsius = round((fahrenheit - 32) * (5 / 9), 2)
-        return celsius
+    def celFah(self, celsius: int | float) -> float | str:
+        try:
+            celsius = float(celsius)
+            fahrenheit = round((celsius * (9 / 5)) + 32, 2)
+            return fahrenheit
+        except ValueError:
+            return self.error
 
-    def celKel(self, celsius: float) -> float:
-        kelvin = round(celsius + 273.15, 2)
-        return kelvin
+    def fahCel(self, fahrenheit: int | float) -> float | str:
+        try:
+            fahrenheit = float(fahrenheit)
+            celsius = round((fahrenheit - 32) * (5 / 9), 2)
+            return celsius
+        except ValueError:
+            return self.error
 
-    def kelCel(self, kelvin: float) -> float:
-        celsius = round(kelvin - 273.15, 2)
-        return celsius
+    def celKel(self, celsius: int | float) -> float | str:
+        try:
+            celsius = float(celsius)
+            kelvin = round(celsius + 273.15, 2)
+            return kelvin
+        except ValueError:
+            return self.error
 
-    def fahKel(self, fahrenheit: float) -> float:
-        kelvin = round((fahrenheit - 32) * (5 / 9) + 273.15, 2)
-        return kelvin
+    def kelCel(self, kelvin: int | float) -> float | str:
+        try:
+            kelvin = float(kelvin)
+            celsius = round(kelvin - 273.15, 2)
+            return celsius
+        except ValueError:
+            return self.error
 
-    def kelFah(self, kelvin: float) -> float:
-        fahrenheit = round((kelvin * 9 / 5) - 459.67, 2)
-        return fahrenheit
+    def fahKel(self, fahrenheit: int | float) -> float | str:
+        try:
+            fahrenheit = float(fahrenheit)
+            kelvin = round((fahrenheit - 32) * (5 / 9) + 273.15, 2)
+            return kelvin
+        except ValueError:
+            return self.error
+
+    def kelFah(self, kelvin: int | float) -> float | str:
+        try:
+            kelvin = float(kelvin)
+            fahrenheit = round((kelvin * 9 / 5) - 459.67, 2)
+            return fahrenheit
+        except ValueError:
+            return self.error
